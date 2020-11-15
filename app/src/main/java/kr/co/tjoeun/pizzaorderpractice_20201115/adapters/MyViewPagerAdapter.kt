4b8a52a53d3a0b0e06bbd00aff4b9c6b1d8b9b42
say-102 <say70007@naver.com>
2020-11-15 15:59:25 +0900

    페이지별 제목 설정

--- app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/adapters/MyViewPagerAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/adapters/MyViewPagerAdapter.kt
@@ -7,6 +7,14 @@ import kr.co.tjoeun.pizzaorderpractice_20201115.fragments.MyProfileFragment
 import kr.co.tjoeun.pizzaorderpractice_20201115.fragments.PizzaStoreFragment
 
 class MyViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> "피자 주문"
+            else -> "내 정보 설정"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
         return when(position) {
             0 -> PizzaStoreFragment()
