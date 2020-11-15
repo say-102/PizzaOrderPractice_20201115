55de998dc87ebe4b915ee2b44ba0cb36dcd73b68
say-102 <say70007@naver.com>
2020-11-15 15:22:48 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/pizzaorderpractice_20201115/MainActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.pizzaorderpractice_20201115
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.tjoeun.lifecycle_20201114.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
